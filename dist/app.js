2f44d2ebca3d9191628c2b042098e3dc2ed74fb0
Samuel Silva <samuel7silva@outlook.com>
2023-03-24 21:10:55 -0300

    test: update build

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,9 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }// import dotenv from 'dotenv';
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
 require('./database');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-// import cors from 'cors';
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 // eslint-disable-next-line import/no-extraneous-dependencies
 var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
@@ -13,18 +13,18 @@ var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interop
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _imageRoutes = require('./routes/imageRoutes'); var _imageRoutes2 = _interopRequireDefault(_imageRoutes);
 
-// dotenv.config();
+_dotenv2.default.config();
 
-// const whiteList = [process.env.FRONTEND_URL];
-// const corsOptions = {
-//   origin(origin, callback) {
-//     if (whiteList.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
+const whiteList = [process.env.FRONTEND_URL];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -43,7 +43,7 @@ class App {
   }
 
   routes() {
-    // this.app.use(cors(corsOptions));
+    this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_expressdelay2.default.call(void 0, 200));
     this.app.use('/', _homeRoutes2.default);
